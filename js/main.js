e2550bedba750757726c27a81669a1cded6fd490
Fabio Junior Barbosa <fabiojbarbosa1@gmail.com>
2021-06-22 22:53:11 -0300

    criar cotacoes

--- js/main.js
+++ js/main.js
@@ -21,3 +21,14 @@ function sem_dados(){
 function setColorIcon(button, className){
     button.querySelector("i").setAttribute("class",className);
 }
+
+/* mostrar e esconder cotaçoes */
+function cotacoesMostrar(id){
+    const ids = ["container-acoes", "container-indices", "container-commodities", "container-forex", "container-criptomoeda"];
+    
+    for (const c in ids) {
+        if(ids[c] == id) document.getElementById(ids[c]).style.display = 'block';
+        else document.getElementById(ids[c]).style.display = 'none';
+    }
+}
+cotacoesMostrar("container-acoes");
